c8f0f2ec0762ea78173bbb0d5f19bef00eb17237
ikth-kim <ikth.kim@gmail.com>
2025-12-18 17:17:14 +0900

    2025-12-18 김태형 jstl 수업

---
+++ 04_KimTaeHyung/chap09-el-jstl-lecture-source/src/main/java/com/kth/chap09eljstllecturesource/el/controller/TestFourServlet.java
@@ -0,0 +1,36 @@
+package com.kth.chap09eljstllecturesource.el.controller;
+
+import com.kth.chap09eljstllecturesource.el.model.dto.MemberDTO;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+
+import java.awt.*;
+import java.io.IOException;
+
+@WebServlet("/test4")
+public class TestFourServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    MemberDTO requestMember = new MemberDTO("홍길동", 19, "010-1234-1234", "gdhong@gmail.com");
+
+    MemberDTO sessionMember = new MemberDTO("이순신", 30, "010-5678-5678", "sslee@gmail.com");
+
+    /* request scope 내장 객체에 속성 추가 */
+    request.setAttribute("member", requestMember);
+
+    /* request scope 내장 객체에 속성 추가 */
+    HttpSession session = request.getSession();
+    session.setAttribute("member", sessionMember);
+
+    RequestDispatcher rd = request.getRequestDispatcher("/views/el/testEl4.jsp");
+    rd.forward(request, response);
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+}
